bfef9fecf8d14a62ceb4651e79315de48a35520e
brent-cybrid <brent@cybrid.app>
2022-05-11 15:53:07 +0000

    v0.7.0

--- api/verificationKeys.service.ts
+++ api/verificationKeys.service.ts
@@ -2,7 +2,7 @@
  * Cybrid Bank API
  * # Welcome  Welcome to the Cybrid platform; enabling turnkey crypto banking services!  In these documents, you will find information on the operations provided by our platform, as well as details on how our REST API operates more generally.  Our complete set of APIs allows you to manage all your resources: your Organization, your banks and your identities. The complete set of APIs can be found on the following pages:  | API                                                            | Description                  | |----------------------------------------------------------------|------------------------------| | [Organization API](https://organization.demo.cybrid.app/api/schema/swagger-ui) | APIs to manage organizations | | [Bank API](https://bank.demo.cybrid.app/api/schema/swagger-ui)                 | APIs to manage banks         | | [Identities API](https://id.demo.cybrid.app/api/schema/swagger-ui)                     | APIs to manage identities    |  When you\'re ready, [request access](https://www.cybrid.xyz/access) to your Dashboard to view and administer your Organization. Once you\'ve logged in, you can begin creating Banks, either for sandbox or production usage, and start enabling your customers to leverage DeFi and web3 with confidence.  If you have any questions, please contact [Support](mailto:support@cybrid.app) at any time so that we can help.  ## Authentication  The Cybrid Platform uses OAuth 2.0 Bearer Tokens to authenticate requests to the platform. Credentials to create Organization and Bank tokens can be generated via your Dashboard ([request access](https://www.cybrid.xyz/access)).  An Organization Token applies broadly to the whole Organization and all of its Banks, whereas, a Bank Token is specific to an individual Bank.  Both Organization and Bank tokens can be created using the OAuth Client Credential Grant flow. Each Organization and Bank has its own unique Client ID and Secret that allows for machine-to-machine authentication.  **Never share your Client ID or Secret publicly or in your source code repository**  Your Client ID and Secret can be exchanged for a time-limited Bearer Token by interacting with the Cybrid Identity Provider or through interacting with the **Authorize** button in this document:  ``` curl -X POST https://id.demo.cybrid.app/oauth/token -d \'{     \"grant_type\": \"client_credentials\",     \"client_id\": \"<Your Client ID>\",     \"client_secret\": \"<Your Secret>\",     \"scope\": \"<Your requested scopes -- space separated>\"   }\' -H \"Content-Type: application/json\" ```  ## Scopes  The Cybrid platform supports the use of scopes to control the level of access a token is limited to. Scopes do not grant access to resources; instead, they provide limits, in support of the least privilege principal.  The following scopes are available on the platform and can be requested when generating either an Organization or a Bank token. Generally speaking, the _Read_ scope is required to read and list resources, the _Write_ scope is required to update a resource and the _Execute_ scope is required to create a resource.  | Resource      | Read scope         | Write scope          | Execute scope     | Token Type         | |---------------|--------------------|----------------------|-------------------|--------------------| | Organizations | organizations:read | organizations:write  |                   | Organization/ Bank | | Banks         | banks:read         | banks:write          | banks:execute     | Organization/ Bank | | Customers     | customers:read     | customers:write      | customers:execute | Bank               | | Assets        | prices:read        |                      |                   | Bank               | | Accounts      | accounts:read      |                      | accounts:execute  | Bank               | | Prices        | prices:read        |                      |                   | Bank               | | Symbols       | prices:read        |                      |                   | Bank               | | Quotes        | quotes:read        |                      | quotes:execute    | Bank               | | Trades        | trades:read        |                      | trades:execute    | Bank               |  ## Organizations  An Organization is meant to model the organization partnering with Cybrid to use our platform.  An Organization does not directly interact with customers. Instead, an Organization has one or more banks, which encompass the financial service offerings of the platform.  ## Banks  A Bank is owned by an Organization and can be thought of as an environment or container for Customers and product offerings. An example of a Bank would be your customer facing banking website, or an internal staging environment for testing and integration.  An Organization can have multiple banks, in sandbox or production environments. A sandbox Bank will be backed by stubbed data and process flows. For instance, identity record and funding source processes will be simulated rather than performed.  ## Customers  Customers represent your banking users on the platform. At present, we offer support for Individuals as Customers.  Customers must be verified in our system before they can play any part on the platform. See the Identity Records section for more details on how a customer can be verified.  Customers must also have an account to be able to transact. See the Accounts APIs for more details on setting up accounts for the customer. 
  *
- * The version of the OpenAPI document: v0.6.17
+ * The version of the OpenAPI document: v0.7.0
  * Contact: support@cybrid.app
  *
  * NOTE: This class is auto generated by OpenAPI Generator (https://openapi-generator.tech).
@@ -94,18 +94,14 @@ export class VerificationKeysService {
     /**
      * Create VerificationKey
      * Creates a verification key.   Example code (python) for generating a Verification Key  &#x60;&#x60;&#x60;python import base64  from cryptography.hazmat.primitives import hashes from cryptography.hazmat.primitives import serialization from cryptography.hazmat.primitives.asymmetric import padding from cryptography.hazmat.primitives.asymmetric import rsa  nonce &#x3D; \&quot;wen moon\&quot; private_key &#x3D; rsa.generate_private_key(public_exponent&#x3D;65537, key_size&#x3D;2048) signature &#x3D; base64.b64encode(private_key.sign(     data&#x3D;nonce.encode(\&#39;ascii\&#39;), padding&#x3D;padding.PKCS1v15(), algorithm&#x3D;hashes.SHA512())).decode(\&#39;ascii\&#39;) public_key &#x3D; base64.b64encode(private_key.public_key().public_bytes(     encoding&#x3D;serialization.Encoding.DER, format&#x3D;serialization.PublicFormat.SubjectPublicKeyInfo)).decode(\&#39;ascii\&#39;) &#x60;&#x60;&#x60;&#x60;  ## State  | State | Description | |-------|-------------| | storing | The Platform is storing the verification in our private key store | | pending | The Platform is verifying the verification key\&#39;s signature | | verified | The Platform has verified the verification key\&#39;s signature and the key can be used | | failed | The Platform was not able to verify the verification key\&#39;s signature and the key cannot be used |    Required scope: **banks:write**
-     * @param bankGuid Identifier for the bank.
      * @param postVerificationKeyBankModel 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
      */
-    public createVerificationKey(bankGuid: string, postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyBankModel>;
-    public createVerificationKey(bankGuid: string, postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyBankModel>>;
-    public createVerificationKey(bankGuid: string, postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyBankModel>>;
-    public createVerificationKey(bankGuid: string, postVerificationKeyBankModel: PostVerificationKeyBankModel, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
-        if (bankGuid === null || bankGuid === undefined) {
-            throw new Error('Required parameter bankGuid was null or undefined when calling createVerificationKey.');
-        }
+    public createVerificationKey(postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyBankModel>;
+    public createVerificationKey(postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyBankModel>>;
+    public createVerificationKey(postVerificationKeyBankModel: PostVerificationKeyBankModel, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyBankModel>>;
+    public createVerificationKey(postVerificationKeyBankModel: PostVerificationKeyBankModel, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
         if (postVerificationKeyBankModel === null || postVerificationKeyBankModel === undefined) {
             throw new Error('Required parameter postVerificationKeyBankModel was null or undefined when calling createVerificationKey.');
         }
@@ -163,7 +159,7 @@ export class VerificationKeysService {
             }
         }
 
-        return this.httpClient.post<VerificationKeyBankModel>(`${this.configuration.basePath}/api/banks/${encodeURIComponent(String(bankGuid))}/verification_keys`,
+        return this.httpClient.post<VerificationKeyBankModel>(`${this.configuration.basePath}/api/bank_verification_keys`,
             postVerificationKeyBankModel,
             {
                 context: localVarHttpContext,
@@ -179,18 +175,14 @@ export class VerificationKeysService {
     /**
      * Get VerificationKey
      * Retrieves a verification key.  Required scope: **banks:read**
-     * @param bankGuid Identifier for the bank.
      * @param verificationKeyGuid Identifier for the verification key.
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
      */
-    public getVerificationKey(bankGuid: string, verificationKeyGuid: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyBankModel>;
-    public getVerificationKey(bankGuid: string, verificationKeyGuid: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyBankModel>>;
-    public getVerificationKey(bankGuid: string, verificationKeyGuid: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyBankModel>>;
-    public getVerificationKey(bankGuid: string, verificationKeyGuid: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
-        if (bankGuid === null || bankGuid === undefined) {
-            throw new Error('Required parameter bankGuid was null or undefined when calling getVerificationKey.');
-        }
+    public getVerificationKey(verificationKeyGuid: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyBankModel>;
+    public getVerificationKey(verificationKeyGuid: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyBankModel>>;
+    public getVerificationKey(verificationKeyGuid: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyBankModel>>;
+    public getVerificationKey(verificationKeyGuid: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
         if (verificationKeyGuid === null || verificationKeyGuid === undefined) {
             throw new Error('Required parameter verificationKeyGuid was null or undefined when calling getVerificationKey.');
         }
@@ -239,7 +231,7 @@ export class VerificationKeysService {
             }
         }
 
-        return this.httpClient.get<VerificationKeyBankModel>(`${this.configuration.basePath}/api/banks/${encodeURIComponent(String(bankGuid))}/verification_keys/${encodeURIComponent(String(verificationKeyGuid))}`,
+        return this.httpClient.get<VerificationKeyBankModel>(`${this.configuration.basePath}/api/bank_verification_keys/${encodeURIComponent(String(verificationKeyGuid))}`,
             {
                 context: localVarHttpContext,
                 responseType: <any>responseType_,
@@ -254,19 +246,15 @@ export class VerificationKeysService {
     /**
      * Get Verification Keys list
      * Retrieves a listing of verification keys of a bank.  Required scope: **banks:read**
-     * @param bankGuid Identifier for the bank.
      * @param page 
      * @param perPage 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
      */
-    public listVerificationKeys(bankGuid: string, page?: number, perPage?: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyListBankModel>;
-    public listVerificationKeys(bankGuid: string, page?: number, perPage?: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyListBankModel>>;
-    public listVerificationKeys(bankGuid: string, page?: number, perPage?: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyListBankModel>>;
-    public listVerificationKeys(bankGuid: string, page?: number, perPage?: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
-        if (bankGuid === null || bankGuid === undefined) {
-            throw new Error('Required parameter bankGuid was null or undefined when calling listVerificationKeys.');
-        }
+    public listVerificationKeys(page?: number, perPage?: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<VerificationKeyListBankModel>;
+    public listVerificationKeys(page?: number, perPage?: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpResponse<VerificationKeyListBankModel>>;
+    public listVerificationKeys(page?: number, perPage?: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<HttpEvent<VerificationKeyListBankModel>>;
+    public listVerificationKeys(page?: number, perPage?: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext}): Observable<any> {
 
         let localVarQueryParameters = new HttpParams({encoder: this.encoder});
         if (page !== undefined && page !== null) {
@@ -322,7 +310,7 @@ export class VerificationKeysService {
             }
         }
 
-        return this.httpClient.get<VerificationKeyListBankModel>(`${this.configuration.basePath}/api/banks/${encodeURIComponent(String(bankGuid))}/verification_keys`,
+        return this.httpClient.get<VerificationKeyListBankModel>(`${this.configuration.basePath}/api/bank_verification_keys`,
             {
                 context: localVarHttpContext,
                 params: localVarQueryParameters,
